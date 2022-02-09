986c5de62fae246fb7cc1dad011437159d0b4c7d
Omar Cely <ocely@unal.edu.co>
2022-02-09 18:28:14 -0500

    vida setentahijueputa

--- src/api/temp-data/routes/temp-data.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * temp-data router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::temp-data.temp-data');
